87e879bc1086ac4ff1af4e50907c11f5b7d82f83
evgenij <zhenyasmagin@yandex.ru>
2018-07-07 10:51:43 +0300

    Add some example with servlet

---
+++ Serblets/src/main/java/FirstServlet.java
@@ -0,0 +1,28 @@
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class FirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletOutputStream out = resp.getOutputStream();
+        out.print("<html>");
+        out.println("<html>");
+        out.println("<head><title>Hello Servlet</title></head>");
+
+        out.println("<body>");
+        out.println("<h3>Hello World</h3>");
+        out.println("This is my first Servlet");
+        out.println("</body>");
+        out.println("<html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+
+    }
+}
---
+++ Serblets/web/init.jsp
@@ -0,0 +1,24 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: evgenij
+  Date: 07.07.2018
+  Time: 10:44
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<%! public void jspInit(){
+    System.out.println("Init jsp");
+};
+public void jspDetroy(){
+    System.out.println("destroy jsp");
+}
+
+
+%>
+</body>
+</html>
